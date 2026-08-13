a21419775b46034cb861d5d2a657f4ae1c7e0422
zeamanuel <mebitzeamanuel@gmail.com>
2026-08-14 02:16:43 +0300

    chore: set up tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import './theme/globals.css';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
